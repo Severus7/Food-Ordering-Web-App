9faaf032747e920f76f08aace121a21a9109c1bf
Severus7 <joshuasapz@gmail.com>
2022-02-10 09:57:04 +0800

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Footer from "./components/Footer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
